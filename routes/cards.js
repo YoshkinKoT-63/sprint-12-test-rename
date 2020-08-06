8463bbafd35484b397739006f36c3495e56604c4
YoshkinKoT-63 <yoshkinkot63@gmail.com>
2020-08-07 00:55:48 +0400

    added promise for users, cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,18 @@
 const cards = require('express').Router();
-const fs = require('fs');
+const fs = require('fs').promises;
+
 const path = require('path');
 
-const filepath = path.join(__dirname, '../data/cards.json');
+const filePath = path.join(__dirname, '../data/cards.json');
 
 cards.get('/', (req, res) => {
-  res.send(JSON.parse(fs.readFileSync(filepath, { encoding: 'utf8' })));
+  fs.readFile(filePath, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(JSON.parse(data));
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'запрашиваемый файл не найден' });
+    });
 });
 
 module.exports = cards;
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,32 @@
 const users = require('express').Router();
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-const filepath = path.join(__dirname, '../data/users.json');
-
-const usersData = JSON.parse(fs.readFileSync(filepath, { encoding: 'utf8' }));
+const filePath = path.join(__dirname, '../data/users.json');
 
 users.get('/', (req, res) => {
-  res.send(usersData);
+  fs.readFile(filePath, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(JSON.parse(data));
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'запрашиваемый файл не найден' });
+    });
 });
 
 users.get('/:id', (req, res) => {
-  const userData = usersData.find((user) => user._id === req.params.id);
-  if (userData) {
-    res.send(userData);
-  } else {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-  }
+  fs.readFile(filePath, { encoding: 'utf8' })
+    .then((data) => {
+      const userData = JSON.parse(data).find((user) => user._id === req.params.id);
+      if (userData) {
+        res.send(userData);
+      } else {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'запрашиваемый файл не найден' });
+    });
 });
 
 module.exports = users;
